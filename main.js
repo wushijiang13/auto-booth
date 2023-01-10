de7a272cef7d6b4316c5b2fe1803985590e2fc55
wushijiang <wushijiang@huan.tv>
2023-01-10 18:40:49 +0800

    feat: 更新模型渲染清晰度，优化层级，增加注释

--- main.js
+++ main.js
@@ -3,47 +3,59 @@ import * as THREE from 'three';
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-// init
 // 相机参数和视角
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 100 );
-camera.position.z = 1;
-camera.position.x = 4;
-camera.position.y = 1;
+function init(){
+    const scene = new THREE.Scene();
 
-const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 100 );
+    camera.position.set(5,1,1);
 
-//点光灯 曝光10
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 5 );
-directionalLight.castShadow = true;
-scene.add( directionalLight );
 
-//加载模型
-const gltfLoader = new GLTFLoader();
-gltfLoader.load('model/car/scene.gltf',function (gltf){
-    scene.add( gltf.scene );
-},function (err){
-    console.log(err);
-})
+    var ambientLight = new THREE.AmbientLight(0xffffff,5);
+    scene.add(ambientLight);
+    //点光灯 曝光5
+    const directionalLight = new THREE.DirectionalLight( 0xffffff, 5 );
+    directionalLight.castShadow = true;
+    scene.add( directionalLight );
 
-const renderer = new THREE.WebGLRenderer({
-    antialias:true,
-    alpha:true,
-    precision:"highp"//着色精度
-});
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animation );
-//设置背景颜色
-renderer.setClearColor(0x2b2b3d,0.5)
-renderer.outputEncoding = THREE.sRGBEncoding;
-document.body.appendChild( renderer.domElement );
 
-const controls = new OrbitControls(camera,renderer.domElement);
-// animation
+    //加载模型
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load('model/car/scene.gltf',function (gltf){
+        // gltf.scene.traverse((obj)=>{
+        //     if(obj.type == 'Mesh'){
+        //         obj.material = new THREE.MeshLambertMaterial({
+        //             map:obj.material.map,
+        //             color:obj.material.color
+        //         });
+        //     }
+        // })
+        scene.add( gltf.scene );
+    },undefined,function (err){
+        console.log(err);
+    })
 
-function animation( ) {
-    //可拖拽视角
-    controls.update();
-    renderer.render( scene, camera );
+    const renderer = new THREE.WebGLRenderer({
+        antialias:true,
+        alpha:true,
+        precision:"highp"//着色精度
+    });
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setAnimationLoop(()=>{
+        //可拖拽视角
+        controls.update();
+        renderer.render( scene, camera );
+    });
+    //设置背景颜色
+    renderer.setClearColor(0x2b2b3d,1)
+    //设置设备像素比。通常用于避免HiDPI设备上绘图模糊
+    renderer.setPixelRatio(window.devicePixelRatio);
+    //将输出canvas的大小调整为(width, height)并考虑设备像素比
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    document.body.appendChild( renderer.domElement );
+    const controls = new OrbitControls(camera,renderer.domElement);
 }
 
-
+init()
